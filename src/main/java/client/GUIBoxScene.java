ea04b48100cb27b270e69a1cf98aef8663030b55
kdports <kdports@gmail.com>
2021-10-13 20:43:55 -0400

    Split start method into helpers
    Split the start method into helpers in order to facilitate future expansion without cluttering a single method

--- src/main/java/client/GUIBoxScene.java
+++ src/main/java/client/GUIBoxScene.java
@@ -16,13 +16,13 @@ import java.io.IOException;
 public class GUIBoxScene extends Application
 {
     int idControl = 0;
+    Pane root = new Pane();
     private double mouseAnchorX;
     private double mouseAnchorY;
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Pane root = new Pane();
-        Button btnAddSlide = new Button();
+        // Pane root = new Pane();
         StackPane holder = new StackPane();
         Canvas canvas = new Canvas(5000,  5000);
         holder.getChildren().add(canvas);
@@ -32,7 +32,26 @@ public class GUIBoxScene extends Application
         primaryStage.setMinHeight(400);
         primaryStage.setMinWidth(500);
 
-        btnAddSlide.setText("Add Slide");
+        Button btnSlide = this.slideButton("Add Slide");
+        root.getChildren().add(btnSlide);
+
+        Button btnPlaytest = this.playtestButton("Playtest");
+        root.getChildren().add(btnPlaytest);
+
+        Button btnSave = this.saveButton("Save");
+        root.getChildren().add(btnSave);
+
+        Scene window = new Scene(root, 1920, 1080);
+        //DECIDES BACKGROUND COLOUR
+        holder.setStyle("-fx-background-color: #2d142c");
+        primaryStage.setTitle("Text Studio");
+        primaryStage.setScene(window);
+        primaryStage.show();
+    }
+
+    public Button slideButton(String name) {
+        Button btnAddSlide = new Button();
+        btnAddSlide.setText(name);
         btnAddSlide.setId("Button " + idControl);
         btnAddSlide.setLayoutX(10);
         btnAddSlide.setLayoutY(10);
@@ -41,30 +60,29 @@ public class GUIBoxScene extends Application
         //DECIDES BUTTON COLOUR
         btnAddSlide.setStyle("-fx-background-color: #c72c41; ");
         btnAddSlide.setOnAction(new SlideHandler(idControl, root));
-        
-        Button btnPlaytest = new Button("Playtest");
+        return btnAddSlide;
+    }
+
+    public Button playtestButton(String name) {
+        Button btnPlaytest = new Button(name);
         btnPlaytest.setStyle("-fx-background-color: #c72c41;");
         btnPlaytest.setLayoutX(10);
         btnPlaytest.setLayoutY(112.5);
         btnPlaytest.setPrefWidth(70);
         btnPlaytest.setPrefHeight(90);
         btnPlaytest.setOnAction(new TestHandler());
+        return btnPlaytest;
+    }
 
-        Button btnSave = new Button("Save");
+    public Button saveButton(String name) {
+        Button btnSave = new Button(name);
         btnSave.setStyle("-fx-background-color: #c72c41;");
         btnSave.setLayoutX(10);
         btnSave.setLayoutY(215);
         btnSave.setPrefWidth(70);
         btnSave.setPrefHeight(90);
         btnSave.setOnAction(new SaveHandler());
-
-        root.getChildren().addAll(btnAddSlide, btnPlaytest, btnSave);
-        Scene window = new Scene(root, 1920, 1080);
-        //DECIDES BACKGROUND COLOUR
-        holder.setStyle("-fx-background-color: #2d142c");
-        primaryStage.setTitle("Text Studio");
-        primaryStage.setScene(window);
-        primaryStage.show();
+        return btnSave;
     }
 
     /**
