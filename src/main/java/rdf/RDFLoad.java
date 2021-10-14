b7019d4a5be7f3e8a08075156b02d8800cd46f7f
Kaspar Poland <kaspar78@mouco.com>
2021-10-13 20:56:51 -0400

    Create first draft of RDF-injecting

--- src/main/java/rdf/RDFLoad.java
+++ src/main/java/rdf/RDFLoad.java
@@ -1,15 +1,79 @@
 package rdf;
 import entities.*;
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.vocabulary.*;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public abstract class RDFLoad {
-    public void loadFromTrig(String filepath) {
-        // Maybe set filepath to default??? no options??
-        // Do some loading
+    private Model model;
+    public RDFLoad(String filepath) throws FileNotFoundException {
+        this.model = ModelFactory.createDefaultModel();
+        this.model.setNsPrefixes(Ontology.prefixes);
+
+        // Read the TTL file into model
+        FileInputStream in = new FileInputStream(filepath);
+        this.model.read(in,null,"TTL");
+    }
+
+    private ArrayList<Decision> getDecisionsOfSlide(Resource slideNode) {
+        // Get the decisions hanging off of param slideNode
+        NodeIterator iter = this.model.listObjectsOfProperty(slideNode, TGEO.hasDecision);
+        ArrayList<RDFNode> results = (ArrayList<RDFNode>) iter.toList();
+
+        // For each decision, build a Decision instance
+        ArrayList<Decision> decisions = new ArrayList<>();
+        for (RDFNode decisionNode : results) {
+            // Get decision text
+            if (decisionNode instanceof Resource) {
+                String decisionText = ((Resource) decisionNode).getProperty(TGEO.hasText).getString();
+                Decision d = new Decision(decisionText);
+                decisions.add(d);
+            }
+        }
+        return decisions;
+    }
+
+    public void loadFromTrig(String filepath) throws FileNotFoundException {
         Game game = new Game();
-        // game.addDeicsion();
-        // game.addSlide();
+
+        // Find and add all Slides
+        ResIterator iter = this.model.listResourcesWithProperty(RDF.type, TGEO.Slide);
+        if (iter.hasNext()) {
+            System.out.println("The database contains slides for:");
+            while (iter.hasNext()) {
+                Resource slideNode = iter.nextResource();
+                String slideText = slideNode.getProperty(TGEO.hasText).getString();
+
+                ArrayList<Decision> decisions = getDecisionsOfSlide(slideNode);
+                Slide slide = new Slide(slideText, decisions);
+
+                // Add the slide as the target after the fact
+                for (Decision d : decisions) {
+                    d.addTarget(slide);
+                }
+
+                // Add as first slide if there aren't slides yet
+                if (game.firstSlide == null) {
+                    game.firstSlide = slide;
+                }
+                game.addSlide(slide);
+            }
+        } else {
+            System.out.println("No slides were found in the database");
+        }
+
         sendGame(game);
     }
 
     public abstract void sendGame(Game game);
+
+    public static void main(String[] args) throws FileNotFoundException {
+        // For debug purposes. Just run this, and if Player.playGame() is hooked up correctly, it should work.
+        String rdfFilepath = "src/main/java/rdf/scratch_game.ttl";
+        RDFLoad loader = new RDFLoadToPlayer(rdfFilepath);
+        loader.loadFromTrig(rdfFilepath);
+    }
 }
--- src/main/java/rdf/RDFLoadToStudio.java
+++ src/main/java/rdf/RDFLoadToStudio.java
@@ -1,7 +1,13 @@
 package rdf;
 import entities.*;
 
+import java.io.FileNotFoundException;
+
 public class RDFLoadToStudio extends RDFLoad {
+    public RDFLoadToStudio(String filepath) throws FileNotFoundException {
+        super(filepath);
+    }
+
     @Override
     public void sendGame(Game game) {
         Studio.loadGame(game);
