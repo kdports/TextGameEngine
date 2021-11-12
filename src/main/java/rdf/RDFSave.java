83012ca278cce798cf177d98bebe315d1795bf22
Kaspar Poland <kaspar78@mouco.com>
2021-11-12 11:09:28 -0500

    Configure packages to get RDFSave to work correctly.

--- src/main/java/rdf/RDFSave.java
+++ src/main/java/rdf/RDFSave.java
@@ -15,34 +15,24 @@ import java.io.OutputStream;
 import java.util.Map;
 
 public class RDFSave {
-    private Model model;
-
-    public static void main(String[] args) throws FileNotFoundException {
-        String rdfFilepath = "src/main/resources/rdf/scratch_game.ttl";
-        RDFLoadToStudio loader = new RDFLoadToStudio(rdfFilepath);
-        EditorGame loadedEditorGame = loader.loadEditorGameFromFile();
-        RDFSave saver = new RDFSave();
-        saver.saveToTrig(loadedEditorGame);
-    }
-
     public void saveToTrig(EditorGame editorGame) throws FileNotFoundException {
         // Use Jena Apache to save to Trig.
 
         // Create model
-        this.model = ModelFactory.createDefaultModel();
-        this.model.setNsPrefixes(Ontology.prefixes);
+        Model model = ModelFactory.createDefaultModel();
+        model.setNsPrefixes(Ontology.prefixes);
 
         // Add all slides with their ID to the model
         for (Map.Entry<Slide, GuiSlideExperiment> entry : editorGame.getAllEntriesSlide()){
             Slide currSlide = entry.getKey();
-            Resource currSlideNode = this.model.createResource(((Integer) currSlide.getId()).toString());
+            Resource currSlideNode = model.createResource(((Integer) currSlide.getId()).toString());
             currSlideNode.addProperty(RDF.type, TGEO.Slide);
         }
 
         // ^^ with decisions
         for (Map.Entry<Decision, GuiDecisionExperiment> entry : editorGame.getAllEntriesDecision()){
             Decision currDecision = entry.getKey();
-            Resource currDecisionNode = this.model.createResource(((Integer) currDecision.getId()).toString());
+            Resource currDecisionNode = model.createResource(((Integer) currDecision.getId()).toString());
             currDecisionNode.addProperty(RDF.type, TGEO.Decision);
         }
 
@@ -50,13 +40,13 @@ public class RDFSave {
         for (Map.Entry<Slide, GuiSlideExperiment> entry : editorGame.getAllEntriesSlide()){
             Slide currSlide = entry.getKey();
             GuiSlideExperiment guiSlide = entry.getValue();
-            Resource currSlideNode = this.model.getResource(((Integer) currSlide.getId()).toString());
+            Resource currSlideNode = model.getResource(((Integer) currSlide.getId()).toString());
 
             currSlideNode.addProperty(TGEO.hasXLocation, String.valueOf(guiSlide.getLayoutX()));
             currSlideNode.addProperty(TGEO.hasYLocation, String.valueOf(guiSlide.getLayoutY()));
             currSlideNode.addProperty(TGEO.hasText, currSlide.getPrompt());
             for (Decision decision : currSlide.outgoingDecisions){
-                currSlideNode.addProperty(TGEO.hasDecision, this.model.getResource(String.valueOf(decision.id)));
+                currSlideNode.addProperty(TGEO.hasDecision, model.getResource(String.valueOf(decision.id)));
             }
         }
 
@@ -64,18 +54,26 @@ public class RDFSave {
         for (Map.Entry<Decision, GuiDecisionExperiment> entry : editorGame.getAllEntriesDecision()){
             Decision currDecision = entry.getKey();
             GuiDecisionExperiment guiDecision = entry.getValue();
-            Resource currDecisionNode = this.model.getResource(((Integer) currDecision.getId()).toString());
+            Resource currDecisionNode = model.getResource(((Integer) currDecision.getId()).toString());
 
             currDecisionNode.addProperty(TGEO.hasXLocation, String.valueOf(guiDecision.getLayoutX()));
             currDecisionNode.addProperty(TGEO.hasYLocation, String.valueOf(guiDecision.getLayoutY()));
             currDecisionNode.addProperty(TGEO.hasText, currDecision.getText());
-            currDecisionNode.addProperty(TGEO.directsTo, this.model.getResource(String.valueOf(currDecision.target.getId())));
+            currDecisionNode.addProperty(TGEO.directsTo, model.getResource(String.valueOf(currDecision.target.getId())));
 
         }
 
         // Save to file
         OutputStream out = new FileOutputStream("output.ttl");
-        this.model.write(out, "TTL");
+        model.write(out, "Turtle");
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String rdfFilepath = "src/main/resources/rdf/scratch_game.ttl";
+        RDFLoadToStudio loader = new RDFLoadToStudio(rdfFilepath);
+        EditorGame loadedEditorGame = loader.loadEditorGameFromFile();
+        RDFSave saver = new RDFSave();
+        saver.saveToTrig(loadedEditorGame);
     }
 }
 
