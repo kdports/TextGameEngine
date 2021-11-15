44216acef3da4a959281ec049a8c18bc240814ef
kdports <kdports@gmail.com>
2021-11-14 21:50:06 -0500

    Player tests and some documentation

--- src/test/java/entities/PlayerTest.java
+++ src/test/java/entities/PlayerTest.java
@@ -8,16 +8,37 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
 
+/*    private GameRenderer renderer;
+    private Player p;
+    private Game sampleGame;*/
+
     @BeforeEach
     void setUp() {
     }
 
-    // Currently just a placeholder, change once the function in Player is fully implemented.
+    // Additonal tests were added in phase 1 for the expanded functionality of Player
     @Test
     void testCheckChoiceTrue() {
         Decision d = new Decision("test");
         assertTrue(Player.checkChoice(d));
     }
 
+    @Test
+    void testFirstSlideStart() {
+        Game sampleGame = CreateSampleGame.returnGame();
+        GameRenderer renderer = new GameRenderer();
+        Player p = new Player(renderer, sampleGame);
+        p.playGame();
+        assertTrue(p.currentSlide.getPrompt().equals(sampleGame.firstSlide.getPrompt()));
+    }
 
+    @Test
+    void testCheckValidChoices() {
+        Game sampleGame = CreateSampleGame.returnGame();
+        GameRenderer renderer = new GameRenderer();
+        Player p = new Player(renderer, sampleGame);
+        p.playGame();
+        p.checkValidChoices();
+        assertTrue(p.currentValidDecisions.size() == 2);
+    }
 }
