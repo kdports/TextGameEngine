88dcc6835ff26837bde593b64258cac9c3599500
KevinL <kkevin.li@mail.utoronto.ca>
2021-11-29 04:44:25 -0500

    Added a bit of animations to the title screen.

--- src/main/java/client/DisplayGame/TitleScreen.java
+++ src/main/java/client/DisplayGame/TitleScreen.java
@@ -1,36 +1,46 @@
 package client.DisplayGame;
-import entities.Game;
-import entities.Player;
+import javafx.animation.*;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import entities.Slide;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
+import javafx.util.Duration;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static java.lang.Long.MAX_VALUE;
 
 public class TitleScreen extends GameRenderer{
 
     public void displayFirstSlide(){
-        VBox box = new VBox(105);
-        box.setAlignment(Pos.CENTER);
+        VBox box = new VBox(400);
+        box.setAlignment(Pos.TOP_CENTER);
         Label title =  new Label("Title Screen");
         title.setFont(Font.font("Abyssinica SIL", FontWeight.BOLD, FontPosture.REGULAR, 55));
+        title.setAlignment(Pos.TOP_CENTER);
         Button start = createStartButton();
         Button settings = createSettingsButton();
+        Button quit = createQuitButton();
         HBox hBox = new HBox(25);
-        hBox.getChildren().addAll(start, settings);
-        hBox.setAlignment(Pos.CENTER);
-
+        hBox.getChildren().addAll(start, settings, quit);
+        hBox.setAlignment(Pos.BASELINE_CENTER);
+        List<Node> nodes = Arrays.asList(quit, start, settings, title);
+        Duration duration = new Duration(2400);
+        addFadeAnimation(nodes, duration);
         box.getChildren().addAll(title, hBox);
 
         Scene scene=new Scene(box,1200,800);
+        addMoveAnimation(title, scene, duration);
         scene.getStylesheets().add("https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap");
         stage.setScene(scene);
         stage.show();
@@ -45,8 +55,16 @@ public class TitleScreen extends GameRenderer{
 
     private Button createSettingsButton(){
         Button button = createButton();
-        button.setOnAction(arg0 -> System.out.println("menu"));
+        button.setOnAction(arg0 -> System.out.println("menu")); // Temporary
         button.setText("Settings");
+        return button;
+    }
+
+
+    private Button createQuitButton(){
+        Button button = createButton();
+        button.setOnAction(arg0 -> stage.close());
+        button.setText("Quit");
         return button;
     }
 
@@ -60,6 +78,28 @@ public class TitleScreen extends GameRenderer{
         button.setWrapText(true);
         button.setStyle("-fx-background-color: #ADD8E6; -fx-border-width: 2px;");
         return button;
+    }
+
+    private void addButtonAnimation(Button button, Color color){
+
+    }
+
+    private void addFadeAnimation(List<Node> nodes, Duration duration){
+        for (Node node : nodes) {
+            FadeTransition ft = new FadeTransition(duration, node);
+            ft.setFromValue(0);
+            ft.setToValue(1);
+            ft.play();
+        }
+    }
+
+    private void addMoveAnimation(Node title, Scene scene, Duration duration){
+        double sceneWidth = scene.getHeight();
+        TranslateTransition translate = new TranslateTransition();
+        translate.setNode(title);
+        translate.setDuration(duration);
+        translate.setByY(sceneWidth / 2.8);
+        translate.play();
     }
 
 }
